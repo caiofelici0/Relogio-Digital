7d95690db0608649583e9fc7cb5a513e9249fc0f
Caio Felicio <caiofelicio1711@gmail.com>
2023-06-06 12:44:47 -0300

    acabou ??

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Relogio
 
-setInterval(mostrarTempo, 1000);
+let intervalRelogio = setInterval(mostrarTempo, 1000);
 
 function mostrarTempo() {
   let data = new Date();
@@ -22,3 +22,114 @@ mostrarTempo();
 
 // Cronometro
 
+const relogioBtn = document.getElementById("relogio-btn");
+const cronometroBtn = document.getElementById("cronometro-btn");
+const relogio = document.getElementById("relogio");
+const cronometro = document.getElementById("cronometro");
+const menuCronometro = document.getElementById("menu-cronometro");
+const iniciarBtn = document.getElementById("iniciar");
+const continuarBtn = document.getElementById("continuar");
+const pararBtn = document.getElementById("parar");
+const reiniciarBtn = document.getElementById("reiniciar");
+const minCronometro = document.getElementById("min-cronometro");
+const secCronometro = document.getElementById("sec-cronometro");
+const milisecCronometro = document.getElementById("milisec-cronometro");
+
+let intervalCronometro;
+let min = 0;
+let sec = 0;
+let milisec = 0;
+let parado = false;
+
+function iniciarCronometro() {
+
+  intervalCronometro = setInterval(() => {
+    if(!parado){
+      milisec++;
+  
+      if(milisec === 100){
+        milisec = 0;
+        sec++;
+
+        sec = sec < 10 ? sec = "0" + sec : sec;
+        secCronometro.innerText = sec;
+      }
+  
+      if(sec === 60){
+        sec = 0;
+        min++;
+
+        sec = sec < 10 ? sec = "0" + sec : sec;
+        secCronometro.innerText = sec;
+
+        min = min < 10 ? min = "0" + min : min;
+        minCronometro.innerText = min;
+      }
+
+      milisec = milisec < 10 ? milisec = "0" + milisec : milisec;
+      milisecCronometro.innerText = milisec;
+    }
+
+  }, 10);
+}
+
+function resetarCronometro() {
+  parado = false;
+  iniciarBtn.classList.remove("hide");
+  continuarBtn.classList.add("hide");
+
+  min = 0;
+  sec = 0;
+  milisec = 0;
+
+  minCronometro.innerText = "00";
+  secCronometro.innerText = "00";
+  milisecCronometro.innerText = "00";
+  clearInterval(intervalCronometro);
+}
+
+// Eventos
+
+relogioBtn.addEventListener("click", () => {
+  if(relogio.classList.contains("hide")){
+    relogio.classList.toggle("hide");
+    cronometro.classList.toggle("hide");
+    menuCronometro.classList.toggle("hide");
+    relogioBtn.classList.toggle("ativo");
+    cronometroBtn.classList.toggle("ativo");
+
+    resetarCronometro();
+    mostrarTempo();
+    intervalRelogio = setInterval(mostrarTempo, 1000);
+  }
+});
+
+cronometroBtn.addEventListener("click", () => {
+  if(cronometro.classList.contains("hide")){
+    relogio.classList.toggle("hide");
+    cronometro.classList.toggle("hide");
+    menuCronometro.classList.toggle("hide");
+    relogioBtn.classList.toggle("ativo");
+    cronometroBtn.classList.toggle("ativo");
+
+    clearInterval(intervalRelogio);
+  }
+});
+
+iniciarBtn.addEventListener("click", () => {
+  iniciarBtn.classList.add("hide");
+  continuarBtn.classList.remove("hide");
+
+  parado = false;
+  iniciarCronometro();
+});
+
+continuarBtn.addEventListener("click", () => {
+  parado = false;
+});
+
+pararBtn.addEventListener("click", () => {
+  parado = true;
+});
+
+reiniciarBtn.addEventListener("click", resetarCronometro);
